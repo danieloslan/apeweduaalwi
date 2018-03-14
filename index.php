860758f066c436ce410a206c910127d24612214b
danieloslan <daniel.alwi.oslan@student.untan.ac.id>
2018-03-14 12:53:09 +0700

    Latihan 1, halaman perdana

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+	//ini adalah komentar
+	// bagaimana bila
+	// engkau wahai komentar
+	// berpanjang lebar dalam berkata-kata
+
+	echo "Halo dunia<br>";
+	echo "Daniel Alwi<br>";
+	echo "NIM H1101161038<br>";
+ ?>
